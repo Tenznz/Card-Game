5a748a551140ca67bc3ed1b1de0cf20724ae733e
Duk <dhugkar@gmail.com>
2021-11-12 01:32:42 +0530

    Four player game added

--- src/com/bridgelabz/card/DeckOfCards.java
+++ src/com/bridgelabz/card/DeckOfCards.java
@@ -1,9 +1,52 @@
 package com.bridgelabz.card;
 
+import java.util.Random;
+
 public class DeckOfCards {
 
-	public static void main(String[] args) {
-		System.out.print("Welcome to the Card Game !!");
+	String[][] card = new String[4][13];
 
+	public void shuffle() {
+		Random random = new Random();
+		int rand1, rand2, rand3, rand4;
+		String temp;
+		for (int i = 0; i < 100; i++) {
+			rand1 = random.nextInt(4);
+			rand2 = random.nextInt(12);
+			rand3 = random.nextInt(4);
+			rand4 = random.nextInt(12);
+
+			if (card[rand1][rand2] != card[rand3][rand4]) {
+				temp = card[rand1][rand2];
+				card[rand1][rand2] = card[rand3][rand4];
+				card[rand3][rand4] = temp;
+			}
+		}
+	}
+
+	private void displayPlayerCard() {
+
+		int n = 8;
+		for (int i = 0; i < 4; i++) {
+			System.out.print("Player " + (i + 1) + ": [ ");
+			for (int j = 0; j < 8; j++) {
+
+				System.out.print(card[i][j] + " ,");
+			}
+			System.out.println(card[i][n] + " ]");
+		}
+	}
+
+	public static void main(String[] args) {
+		String[] suit = { "Clubs", "Diamonds", "Hearts", "Spades" };
+		String[] rank = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+		DeckOfCards deckOfCards = new DeckOfCards();
+		for (int i = 0; i < suit.length; i++) {
+			for (int j = 0; j < rank.length; j++) {
+				deckOfCards.card[i][j] = suit[i] + rank[j];
+			}
+		}
+		deckOfCards.shuffle();
+		deckOfCards.displayPlayerCard();
 	}
 }
